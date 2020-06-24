b47423e20de4a47b0c70e866d6682fb8011ac547
Admin <Admin@192.168.76.144>
2020-06-24 17:13:01 +0900

    오류수정

--- SS/src/school/controllers/SchoolController.java
+++ SS/src/school/controllers/SchoolController.java
@@ -2,13 +2,14 @@ package school.controllers;
 
 import java.io.IOException;
 import java.util.List;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 import school.models.SchoolDAO;
 import school.models.SchoolDTO;
@@ -16,17 +17,17 @@ import school.models.SchoolDTO;
 @WebServlet("/schoolSignal")
 public class SchoolController extends HttpServlet {
 
+	
 	@Override
-	public void service(ServletRequest request, ServletResponse response) throws ServletException, IOException {
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		SchoolDAO deptDAO = SchoolDAO.getInstance();
 		List<SchoolDTO> list = deptDAO.getDeptList();
 
 		request.setAttribute("list", list);
 		request.setAttribute("title", "학교위치");
 		
-		RequestDispatcher dispatcher = request.getRequestDispatcher("/views/SchoolLocation.jsp");
+		RequestDispatcher dispatcher = request.getRequestDispatcher("/schoolSignal/kakaoAPI.jsp");
 		dispatcher.forward(request, response);
-		
 	}
 
 }
